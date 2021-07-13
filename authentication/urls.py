bc3858261a562739a011aaaa2d8fc327c0434458
rezyte <alirezaalavirazavi@yahoo.com>
2021-07-13 12:51:14 +0430

    first commit

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import RegisterView, LoginView
+
+app_name = "authentication"
+
+urlpatterns = [
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+]
